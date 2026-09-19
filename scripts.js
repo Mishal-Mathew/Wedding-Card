7035e41a1c9f270fc490f969d238a58687a61888
Mishal Mathew <mishalmmathew@gmail.com>
2026-09-20 00:34:47 +0530

    Complete Page 1

--- scripts.js
+++ scripts.js
@@ -3,32 +3,17 @@ gsap.registerPlugin(ScrollTrigger);
 window.addEventListener("load", () => {
     gsap.timeline({
             scrollTrigger: {
-<<<<<<< HEAD
                 trigger: ".first",
                 start: "top top",
                 end: "+=450%",
-=======
-                trigger: ".wrapper",
-                start: 0,
-                end: "+=250%",
->>>>>>> parent of eda4345 (Changed structure)
                 pin: true,
                 scrub: true
             }
         })
-<<<<<<< HEAD
         .to(".foreground",{
             scale:3,
             opacity:0,
             ease:"power1.in",
-=======
-        .to("img",{
-            scale:4,
-            z:350,
-            transformOrigin:"center center",
-            ease:"power1.inOut",
-            opacity:0
->>>>>>> parent of eda4345 (Changed structure)
         })
         .to(".background",{
             scale:2.4,
